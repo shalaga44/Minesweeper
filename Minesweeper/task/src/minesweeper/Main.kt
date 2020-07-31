b32be906ea3f39d594735380b09afc37d70f89cf
Mohamed Moawia <mohamedmoawia4@gmail.com>
2020-07-31 02:17:03 +0200

    Stage #1: Lay the groundwork
    Signed-off-by: Mohamed Moawia <mohamedmoawia4@gmail.com>

--- Minesweeper/task/src/minesweeper/Main.kt
+++ Minesweeper/task/src/minesweeper/Main.kt
@@ -1,5 +1,53 @@
 package minesweeper
 
+class Minesweeper {
+    lateinit var matrix: Array<Array<Int>>
+    private val mineFlag = 0
+    private val safeFlag = 1
+    private val mineCell = 'X'
+    private val safeCell = '.'
+    private val errorCell= '?'
+    private var height = 0
+    private var width = 0
+
+    fun showGame() {
+        matrix.forEach { rows ->
+            rows.forEach { cell ->
+                print(stringCellOf(cell))
+            }
+            println()
+        }
+    }
+
+    private fun stringCellOf(cell: Int): Char {
+        return when(cell){
+            mineFlag  -> mineCell
+            safeFlag -> safeCell
+            else ->  errorCell
+        }
+    }
+
+    fun createNew(height: Int, width: Int, mines: Int) {
+        this.height = height
+        this.width = width
+        matrix = Array(height) { Array(width) { safeFlag } }
+        addNewMinesToMatrix(mines)
+    }
+
+    private fun addNewMinesToMatrix(mines: Int) {
+        repeat(mines) {
+            val randomX = (0 until height).random()
+            val randomY = (0 until width).random()
+            matrix[randomY][randomX] = mineFlag
+
+        }
+    }
+
+}
+
+
 fun main() {
-    print("Hello, World!")
+    val game = Minesweeper()
+    game.createNew(9, 9, 10)
+    game.showGame()
 }

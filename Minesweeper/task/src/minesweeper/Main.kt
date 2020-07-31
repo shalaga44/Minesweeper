09d7e8deaeb35483dd7c02c935176b5658d940f7
Mohamed Moawia <mohamedmoawia4@gmail.com>
2020-07-31 03:14:38 +0200

    Stage #2: Flexible mines
    Signed-off-by: Mohamed Moawia <mohamedmoawia4@gmail.com>

--- Minesweeper/task/src/minesweeper/Main.kt
+++ Minesweeper/task/src/minesweeper/Main.kt
@@ -1,12 +1,13 @@
 package minesweeper
 
 class Minesweeper {
-    lateinit var matrix: Array<Array<Int>>
+    private lateinit var minesList: Map<Pair<Int, Int>, Int>
+    private lateinit var matrix: Array<Array<Int>>
     private val mineFlag = 0
     private val safeFlag = 1
     private val mineCell = 'X'
     private val safeCell = '.'
-    private val errorCell= '?'
+    private val errorCell = '?'
     private var height = 0
     private var width = 0
 
@@ -20,34 +21,48 @@ class Minesweeper {
     }
 
     private fun stringCellOf(cell: Int): Char {
-        return when(cell){
-            mineFlag  -> mineCell
+        return when (cell) {
+            mineFlag -> mineCell
             safeFlag -> safeCell
-            else ->  errorCell
+            else -> errorCell
         }
     }
 
-    fun createNew(height: Int, width: Int, mines: Int) {
+    private fun createNew(mines: Int, height: Int = 9, width: Int = 9) {
         this.height = height
         this.width = width
         matrix = Array(height) { Array(width) { safeFlag } }
+        minesList = HashMap(width * height)
         addNewMinesToMatrix(mines)
     }
 
     private fun addNewMinesToMatrix(mines: Int) {
         repeat(mines) {
-            val randomX = (0 until height).random()
-            val randomY = (0 until width).random()
-            matrix[randomY][randomX] = mineFlag
-
+            while (true) {
+                val pos = (0 until width * height)
+                        .random().toPointIn2D(width, height)
+                if (matrix[pos.second][pos.first] != safeFlag)
+                    continue
+                matrix[pos.second][pos.first] = mineFlag
+                break
+            }
         }
     }
 
+
+    fun askUserToCreate() {
+        println("How many mines do you want on the field?")
+        val mines = readLine()!!.toInt()
+        createNew(mines)
+    }
 }
 
+private fun Int.toPointIn2D(width: Int, height: Int): Pair<Int, Int> {
+    return Pair(this % width, this / height)
+}
 
 fun main() {
     val game = Minesweeper()
-    game.createNew(9, 9, 10)
+    game.askUserToCreate()
     game.showGame()
 }
